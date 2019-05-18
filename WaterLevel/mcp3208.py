7e640a5e5a4bad1f211b02c5cc21ab5794178b93
Otosi <panuphan1997@gmail.com>
2019-05-18 21:43:50 +0700

    test_1

--- WaterLevel/mcp3208.py
+++ WaterLevel/mcp3208.py
@@ -1,16 +1,18 @@
-from mcp3208 import MCP3208
+# from mcp3208 import MCP3208
 import RPi.GPIO as GPIO
 import time
 
 pwmPin = 3
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(3,GPIO.OUT)
-pwm1 = GPIO.PWM(pwmPin,120) #PWM frequency
-pwm1.start(0)
-adc = MCP3208()
+GPIO.setup(3,GPIO.IN)
+# pwm1 = GPIO.PWM(pwmPin,120) #PWM frequency
+# pwm1.start(0)
+# adc = MCP3208()
 
 while(1):
-    print(adc.read(0))
-    pwm1.ChangeDutyCycle(adc.read(0)/40.95)
+    # print(adc.read(0))
+    # pwm1.ChangeDutyCycle(adc.read(0)/40.95)
+    # pwm1.ChangeDutyCycle(adc.read(0))
+    print(">>"+str(GPIO.input(pwmPin)))
     time.sleep(0.5)
