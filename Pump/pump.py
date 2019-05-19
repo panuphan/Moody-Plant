cb07210757a9b8947be461aa1b5e4224a195084d
Otosi <panuphan1997@gmail.com>
2019-05-19 15:00:49 +0700

    cleanup

--- Pump/pump.py
+++ Pump/pump.py
@@ -8,11 +8,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(PUMP_PIN,GPIO.OUT)
 
-while(1):
-    
-    GPIO.output(PUMP_PIN,0)
-    print("ON")
-    time.sleep(3)
-    GPIO.output(PUMP_PIN,1)
-    print("OFF")
-    time.sleep(3)
+try:
+    while(1):
+        
+        GPIO.output(PUMP_PIN,0)
+        print("ON")
+        time.sleep(3)
+        GPIO.output(PUMP_PIN,1)
+        print("OFF")
+        time.sleep(3)
+except:
+    print("Cleanup")
+    GPIO.cleanup()
